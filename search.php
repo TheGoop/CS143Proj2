ac1a63f2d38840c304d242687b52c7b1a04a61bf
Akshay Gupta <akshaygupta.us@gmail.com>
2021-10-24 01:55:16 -0700

    finished project

--- search.php
+++ search.php
@@ -1,6 +1,27 @@
 <!DOCTYPE html>
 <html>
 
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+
 <body>
 
     <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -17,8 +38,13 @@
         <h2>Make A Search!</h2>
         <form method="post">
             <label for="fname">Actor Name</label>
-            <input type="text" name="name" width="400 px">
-            <input type="submit" name="submit" value="Search">
+            <input type="actor" name="actor" width="400 px">
+            <input type="submit" name="submitActor" value="Search">
+            <?php echo "\t\t"; ?>
+            <label for="fname">Movie Name</label>
+            <input type="movie" name="movie" width="400 px">
+            <input type="submit" name="submitMovie" value="Search">
+        </form>
         </form>
     </center>
 </body>
@@ -31,8 +57,12 @@ function redirect($url, $statusCode = 303)
     die();
 }
 
-if (isset($_POST["submit"])) {
-    redirect("./search.php?actor=" . $_POST["name"]);
+if (isset($_POST["submitActor"])) {
+    redirect("./search.php?actor=" . $_POST["actor"]);
+}
+
+if (isset($_POST["submitMovie"])) {
+    redirect("./search.php?movie=" . $_POST["movie"]);
 }
 
 
@@ -41,8 +71,51 @@ if ($db->connect_errno > 0) {
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
 if (isset($_GET["actor"])) {
-    $name = htmlspecialchars($_GET["actor"]);
+    $actor = htmlspecialchars($_GET["actor"]);
+    $keywords = explode(" ", $actor);
     $query = "";
+    for ($i = 0; $i < count($keywords); $i++) {
+        $keyword = $keywords[$i];
+        $defaultQuery = "select first, last, dob, id from Actor where first like '%{$keyword}%' or last like '%{$keyword}%' union ";
+        $query = $query . $defaultQuery;
+    }
+    $query = $query . "select first, last, dob, id from Actor where 1 = 0";
+    $rs = $db->query($query);
+    echo "<br><table>";
+    echo "<tr>
+                <th> Name  </th>
+                <th> Date of Birth </th>
+            </tr>";
+    while ($row = $rs->fetch_assoc()) {
+        $actorUrl = "./actor.php?id=" . $row['id'];
+        echo "<tr>";
+        echo "<th><a href='$actorUrl'>" . $row["first"] . " " . $row["last"] . "</a></th>";
+        echo "<th>" . $row['dob'] . "</th>";
+        echo "</tr>";
+    }
+} else if (isset($_GET["movie"])) {
+    $movie = htmlspecialchars($_GET["movie"]);
+    $keywords = explode(" ", $movie);
+    $query = "";
+    for ($i = 0; $i < count($keywords); $i++) {
+        $keyword = $keywords[$i];
+        $defaultQuery = "select title, year, id from Movie where title like '%{$keyword}%' union ";
+        $query = $query . $defaultQuery;
+    }
+    $query = $query . "select title, year, id from Movie where 1 = 0";
+    $rs = $db->query($query);
+    echo "<br><table>";
+    echo "<tr>
+                <th> Title  </th>
+                <th> Year </th>
+            </tr>";
+    while ($row = $rs->fetch_assoc()) {
+        $movieUrl = "./movie.php?id=" . $row['id'];
+        echo "<tr>";
+        echo "<th><a href='$movieUrl'>" . $row["title"] . "</a></th>";
+        echo "<th><a href='$movieUrl'>" . $row['year'] . "</a></th>";
+        echo "</tr>";
+    }
 }
 
 
