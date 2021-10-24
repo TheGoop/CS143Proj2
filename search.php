9325e803398c18847a0271faee59d4da8c2d4735
Akshay Gupta <akshaygupta.us@gmail.com>
2021-10-24 00:57:21 -0700

    got search submission and storage of actor name to search working, just need to make sql query to search and table to display

--- search.php
+++ search.php
@@ -2,37 +2,51 @@
 <html>
 
 <body>
-    <h1>Make A Search!</h1>
 
-    <form method="post">
-        <label for="fname">Actor Name</label>
-        <input type="text" name="name">
-        <input type="submit" name="submit" value="Search">
-    </form>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header navbar-defalt">
+                <a class="navbar-brand" href="./index.php">Akshay's CS143 DataBase Query System</a>
+            </div>
+        </div>
+    </nav>
+    <h1>
+        <center> Welcome To CS143 Project 2! </center>
+    </h1>
+    <center>
+        <h2>Make A Search!</h2>
+        <form method="post">
+            <label for="fname">Actor Name</label>
+            <input type="text" name="name" width="400 px">
+            <input type="submit" name="submit" value="Search">
+        </form>
+    </center>
 </body>
 
 <?php
 
-if ($_POST['name']) {
-    $db = new mysqli('localhost', 'cs143', '', 'class_db');
-    if ($db->connect_errno > 0) {
-        die('Unable to connect to database [' . $db->connect_error . ']');
-    }
-
-    $id = $_GET['id'];
-    $name = $_POST['name'];
-    $rating = $_POST['rating'];
-    $comment = $_POST['comment'];
-    $query = "insert into Review values ('$name', now(), $id, $rating, '$comment')";
-    $rs = $db->query($query);
-    if ($rs) {
-        $movieUrl = "review.php?id={$row['mid']}";
-        echo "Thanks for your comment! Your review has been successfully added. ";
-        echo "<a href='$movieUrl'>click this to go back to see the movie</a><br>";
-    }
-
-    $db->close();
+function redirect($url, $statusCode = 303)
+{
+    header('Location: ' . $url, true, $statusCode);
+    die();
 }
+
+if (isset($_POST["submit"])) {
+    redirect("./search.php?actor=" . $_POST["name"]);
+}
+
+
+$db = new mysqli('localhost', 'cs143', '', 'class_db');
+if ($db->connect_errno > 0) {
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+if (isset($_GET["actor"])) {
+    $name = htmlspecialchars($_GET["actor"]);
+    $query = "";
+}
+
+
+$db->close();
 ?>
 
 
